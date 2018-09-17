14d054253d3b9ddbc8b00c05c4b1724b09926bb5
LuZhiqing <zhiqing.lu@yinyuetai.com>
2018-09-17 14:29:59 +0800

    eureka整合spring boot2.0

--- src/main/java/vens/ai/graduate/design/notifaction/configs/DataSourceConfigs.java
+++ src/main/java/vens/ai/graduate/design/notifaction/configs/DataSourceConfigs.java
@@ -2,8 +2,8 @@ package vens.ai.graduate.design.notifaction.configs;
 
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
